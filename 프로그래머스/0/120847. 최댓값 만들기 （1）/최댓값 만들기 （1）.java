620d689d9f9004143786191bcc163ad8064602ac
HanKyungPark <156498970+HanKyungPark@users.noreply.github.com>
2024-11-21 21:53:12 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.57 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+    
+    Arrays.sort(numbers);
+    
+    int max = numbers[numbers.length-1] * numbers[numbers.length -2];
+    
+    return max;
+        
+    }
+}
